a413a8e1d99038203cec956ff740550eaad6450a
Carol-Mwangi <carolyne@boyahq.com>
2024-04-12 11:27:43 +0300

    Commited

--- index.js
+++ index.js
@@ -1,90 +1,176 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const {MongoClient} = require('mongodb');
+const passport = require('passport');
+const session = require('express-session');
+const passportLocalMongoose = require('passport-local-mongoose');
 const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
 
-
+//using the body parser
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+    secret: "long secret key",
+    resave: false,
+    saveUninitialized: false
+}));
+
+//initializing passport and starting a session
+app.use(passport.initialize());
+app.use(passport.session()); //starting a session
+
 
 // MongoDB connection URI
 const mongoURI = 'mongodb+srv://carolyne:BgfduSTsMYrXTzWI@lyne.zm3suyw.mongodb.net/carolyne?retryWrites=true&w=majority&appName=Lyne';
 
-// Connect to MongoDB using Mongoose
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(error => console.error('Failed to connect to MongoDB:', error));
- // creating a schema ie the structure of my db data and defining the schema types
-const userSchema = mongoose.Schema({
-    firstName: String,
-    lastName: String,
-    email: String,
-    gender: String,
-    password: String
+//connection using mongoclient
+const dbName = 'Carol';
+const collection = 'NewUsers';
+
+ const client = new MongoClient(mongoURI, {useNewUrlParser: true, useUnifiedTopology: true});
+
+ //check connection to Mongodb
+ client.connect()
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch(err => {
+        console.error('Error connecting to MongoDB:', err);
+    });
+   
+    
+//creating a user
+app.get('/signup', (req, res)=> {
+    res.sendFile(__dirname + '/signup.html')
 });
 
-// Define a Mongoose model 
-const userModel = mongoose.model('User', userSchema);
+app.post('/signup', async (req, res)=>{
+    try { //extract the user data
+        var firstName = req.body.fname;
+        var lastName = req.body.lname;
+        var email = req.body.email;
+        var gender = req.body.gender;
+        var password = req.body.password;
 
-app.get('/save', async (req, res)=>{
- const newUser = userModel({
-    firstName: 'Carol',
-    lastName: 'Mwangi',
-    email: 'wanguicaroline@gmail.com',
-    gender: 'Female',
-    password: 'Mwangi'
- });
-  try {
-    const saveUser = await newUser.save();
-       console.log(saveUser, 'New User created');
-        res.send('Created user');
-    } 
-    catch (error) {
-        console.log(error);
-        res.send('error creating the user')
+        //connect to db
+        await client.connect();
+
+        //access db and collection
+        const db = client.db('Carol');
+        const collection = db.collection('NewUsers');
+//create ner user account
+        const newUser = {
+            firstName,
+            lastName,
+            email,
+            gender,
+            password
+        }
+       await collection.insertOne(newUser);
+        res.redirect('/login')
+        console.log('newUser', newUser);
+    } catch (err){
+        console.error(err);
+        res.send('Error creating the user')
     }
 });
 
-// finding a user from my db 
-app.get('/findAll', async (req,res)=>{
-   try {const users = await userModel.find({email: 'carolyne@boyahq.com'});
-       console.log(users);
-        res.send(users)
-    } 
-    catch(error) {
-        console.log(error)
-        res.send('Error Retrieving data')
-    }
-   });
-
-//updating the data in the db
-   app.get('/update',async(req,res)=>{
-    try {const updateUser = await userModel.findOneAndUpdate({firstName: 'Carol'}, {email : 'caroline@boya.co'});
-    console.log(updateUser);
-    res.send('User Updated Successfully')       
-
-   }
-   catch(error) {
-    console.log(error)
-    res.send('Trouble Updating the user')
-   }
-});
-
-//deleting the user
-app.get('/delete',async(req,res) =>{
+app.get('/login',(req, res)=>{
+    res.sendFile(__dirname +'/login.html')
+})
+//find and login
+app.post('/login', async(req,res)=>{
     try {
-        const deleteUser = await userModel.findOneAndDelete({email: 'lilian@gmail.com'});
-        console.log('deleteUser');
-        res.send(`${deleteUser} successfully deleted`);
-    }
-    catch (err) {
-        console.log(error);
-        res.send('Unable to perfom the operation')
+        const {email, password} =req.body
+        await client.connect();
+
+        const db = client.db('Carol');
+        const collection = db.collection('NewUsers');
+
+        const user = await collection.findOne({email, password});
+
+        if(user) {
+            console.log('login successful')
+            res.send('logged in')
+        } else {
+            res.send('Invalid login details')
+        }
+    } catch(err) {
+        console.error(err);
+        res.send('Error logging in');
     }
 });
+ 
+
+
+
+// Start the Express server
+
+//read operation
+app.get('/users',async(req,res)=>{
+    try {
+        await client.connect();
+
+        const db = client.db('Carol');
+        const collection = db.collection('NewUsers');
+
+        const users = await collection.find({email: 'carolyne@boyahq.com'}).toArray();
+        res.json(users);
+        console.log(users)
+
+    } catch (err) {
+        console.log(err)
+        res.send('Error retrieving users');
+    }
+})
+
+//update user details
+app.get('/reset',(req,res)=>{
+    res.sendFile(__dirname+'/reset.html')
+});
+
+app.post('/reset', async(req,res)=>{
+    try {
+        const {email, password, confirmPassword}=req.body;
+
+        if(password !== confirmPassword) {
+            console.log('password Mismatch');
+            res.send('Password mismatch')
+        }
+
+        await client.connect();
+         const db = client.db('Carol');
+         const collection = db.collection('NewUsers')
+
+         await collection.updateOne({email}, {$set:{password: confirmPassword}});
+         console.log('password reset successfully');
+         res.redirect('/login');
+    } catch (err) {
+        console.error(err);
+        res.send('Error in resetting the password')
+    }
+})
+ app.get('/delete',async(req,res)=>{
+    try {
+        await client.connect();
+        const db = client.db('Carol');
+        const collection = db.collection('NewUsers')
+
+       const deleted = await collection.deleteMany({email:'carolyne@boyahq.com'});
+        console.log (deleted,'User deleted');
+        res.send('User deleted');
+    }  catch (err) {
+        console.log(err);
+        res.send('Trouble deleting the user');
+    }
+ })
+
+
+
+
+
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
-
